439839352333ed2cb805398e2efba10ebdce248d
pavelsof <mail@pavelsof.com>
2023-07-20 15:39:14 +0200

    First exceptions.

--- kabelwerk/__init__.py
+++ kabelwerk/__init__.py
@@ -1,1 +1,4 @@
+from .exceptions import *
+
+
 __version__ = '0.1.0'
---
+++ kabelwerk/exceptions.py
@@ -0,0 +1,33 @@
+class KabelwerkException(Exception):
+    """
+    The base class for all Kabelwerk exceptions.
+    """
+    pass
+
+
+class ConnectionError(KabelwerkException):
+    """
+    Raised when there is an issue with the connection to the Kabelwerk backend.
+    """
+    pass
+
+
+class AuthenticationError(KabelwerkException):
+    """
+    Raised when the authentication token is rejected by the Kabelwerk backend.
+    """
+    pass
+
+
+class ValidationError(KabelwerkException):
+    """
+    Raised when the input data is rejected by the Kabelwerk backend as invalid.
+    """
+    pass
+
+
+class ServerError(KabelwerkException):
+    """
+    Raised when the Kabelwerk backend fails to properly handle the request.
+    """
+    pass
